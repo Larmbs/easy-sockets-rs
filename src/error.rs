cfbdc86ca69d35a8907970ffc8ac892aab29ac52
Liam Israel <138258207+Larmbs@users.noreply.github.com>
2024-09-20 12:52:53 +0300

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,8 @@
  
 
 
-
- pub trait ErrorCode {
+/// Trait definable on enun to define error codes and their messages.
+pub trait ErrorCode {
     fn code(&self) -> u16;
     fn message(&self) -> &'static str;
  }
